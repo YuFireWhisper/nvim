9d8cb580976c5678aef84f9c79bc96587dff35c1
YuFireWhipser <yu.whisper.personal@gmail.com>
2025-03-12 20:38:23 +0800

    feat: add keybinding to copy line diagnostics
    - Add <leader>el mapping to copy diagnostics from current line
    - Format diagnostics with line and column information
    - Provide user feedback via notifications

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -32,6 +32,28 @@ return {
         end,
         desc = "Copy all diagnostics"
       },
+      {
+        "<leader>el",
+        function()
+          local current_line = vim.api.nvim_win_get_cursor(0)[1] - 1
+          local diagnostics = vim.diagnostic.get(0, { lnum = current_line })
+          if #diagnostics > 0 then
+            local texts = {}
+            for _, diagnostic in ipairs(diagnostics) do
+              local line = diagnostic.lnum + 1
+              local col = diagnostic.col + 1
+              local text = string.format("[Line %d, Col %d] %s", line, col, diagnostic.message)
+              table.insert(texts, text)
+            end
+            local diagnostic_text = table.concat(texts, "\n")
+            vim.fn.setreg('+', diagnostic_text)
+            vim.notify("Copied " .. #diagnostics .. " diagnostic messages from current line!")
+          else
+            vim.notify("No diagnostics on current line to copy!")
+          end
+        end,
+        desc = "Copy line diagnostics"
+      },
     },
   }
 }
